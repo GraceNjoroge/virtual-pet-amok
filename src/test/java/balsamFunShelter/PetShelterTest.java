32d226ffe0a13653b26d8963e625c8cbc1ba3540
GraceNjoroge <gkmbogo@gmail.com>
2019-06-02 14:05:59 -0400

    added organic and robotic test classes

--- src/test/java/balsamFunShelter/PetShelterTest.java
+++ src/test/java/balsamFunShelter/PetShelterTest.java
@@ -1,7 +1,7 @@
 package balsamFunShelter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 
